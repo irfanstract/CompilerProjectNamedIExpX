58f68ed330609ac9a3cff530590552b0bf230cc2
me <meh@meh>
2024-03-04 10:21:32 +0700

    make changes like these

--- packages/typexpe-compiler-chain/test/compileExceptionsCase.mjs
+++ packages/typexpe-compiler-chain/test/compileExceptionsCase.mjs
@@ -1,10 +1,30 @@
 
 
+import assert from "assert";
+import { TypeError } from "typexpe-compiler-fwcore/src/compileExceptions.mjs";
+
+
 
 
 
 describe("compile-exceptions", () => {
   ;
+
+  it(`the imported ${TypeError.name } is what we're talking about`, () => {
+    assert(TypeError !== globalThis.TypeError ) ;
+  } ) ;
+  
+  it(`should successfully instantiate ${TypeError.name }`, () => {
+    const e = new TypeError(`Type Mismatch; at L132@<repl>`) ;
+    assert(e instanceof TypeError ) ;
+    console["log"](e) ;
+  } ) ;
+  
+  it(`should successfully instantiate and throw ${TypeError.name }`, () => {
+    const e = new TypeError(`Type Mismatch; at L135@<repl>`) ;
+    assert.throws(() => { throw e ; } ) ;
+  } ) ;
+
 } ) ;
 
 
